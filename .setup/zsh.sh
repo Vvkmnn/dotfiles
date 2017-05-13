2a1dce59d4622bf04e6f51200254e0604f0c72cd
Vivek Menon <mail@vvkmnn.xyz>
2017-05-12 23:13:06 -0400

    Better ZSH setup with zIM

--- .setup/zsh.sh
+++ .setup/zsh.sh
@@ -3,25 +3,22 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Alf
-curl -sSL https://raw.githubusercontent.com/psyrendust/alf/master/bootstrap/baseline.zsh | zsh
+# Zim!
+git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 
-# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Prepend Zim Templates (Already Available)
+# setopt EXTENDED_GLOB
+# for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+#   user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+#   touch ${user_file}
+#   ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
+# done
 
-tell "Switching the login shell to ZSH..."
+# Set Zsh as shell
+chsh -s =zsh
 
-CURRENTSHELL=$(dscl . -read /Users/$USER UserShell | awk '{print $2}')
-if [[ "$CURRENTSHELL" != "/usr/local/bin/zsh" ]]; then
-  warn "setting newer homebrew zsh (/usr/local/bin/zsh) as your shell (password required)"
-  sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
-  chsh -s /usr/local/bin/zsh
-  sudo dscl . -change /Users/$USER UserShell $SHELL /usr/local/bin/zsh > /dev/null 2>&1
-  fin "Switched!"
-fi
-
-tell "Checking if you have the Oh-my-Zsh framework installed..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Source .zlogin
+source ${ZDOTDIR:-${HOME}}/.zlogin
 
 if [[ ! -d "../.oh-my-zsh/custom/themes/dracula" ]]; then
   cp ../.assets/dracula/zsh/dracula.zsh-theme ../.oh-my-zsh/themes/
@@ -29,5 +26,8 @@ fi
 
 fin "All set."
 
- # Source .zshrc for new changes
-source ~/.zshrc
+# Update Zim
+zmanage update
+
+ # Source .zlogin to compile new .zshrc
+source ~/.login
