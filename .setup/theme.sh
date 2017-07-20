c305842b9bcc1db849fefdb9ebf1983e0c396658
Vivek Menon <mail@vvkmnn.xyz>
2017-07-19 21:46:03 -0400

    Alfred theme.

--- .setup/theme.sh
+++ .setup/theme.sh
@@ -4,3 +4,6 @@ git clone https://github.com/dracula/terminal.app.git $HOME/.theme/terminal
 # Dracula for iTerm2
 git clone https://github.com/dracula/iterm.git $HOME/.theme/iterm2
 
+# Dracula for Alfred
+git clone https://github.com/dracula/alfred $HOME/.theme/alfred
+
