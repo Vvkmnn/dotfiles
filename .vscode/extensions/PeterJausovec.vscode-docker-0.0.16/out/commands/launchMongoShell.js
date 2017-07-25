836812e0ed27160b8960a2fca4a6ede2a4ddc16e
Vivek Menon <mail@vvkmnn.xyz>
2017-07-25 16:38:47 -0400

    Vscode settings

--- .vscode/extensions/PeterJausovec.vscode-docker-0.0.16/out/commands/launchMongoShell.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-function launchMongoShell() {
-    let terminal = vscode.window.createTerminal("Mongo", "/usr/local/bin/mongo");
-    terminal.show();
-}
-exports.launchMongoShell = launchMongoShell;
-//# sourceMappingURL=launchMongoShell.js.map
