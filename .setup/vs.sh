ed512191066535fbe392bc9e940fa547bf522cf5
Vivek Menon <mail@vvkmnn.xyz>
2017-07-25 21:18:30 -0400

    Adding Vscode setup,

--- .setup/vs.sh
+++ .setup/vs.sh
@@ -1,2 +1,9 @@
-ln-s ~/.dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-mv ~/Library/Application\ Support/Code/User/settings.json ~/.dotfiles/.vscode
+#!/usr/bin/env bash
+
+# if [ ! -f ~/Library/Application\ Support/Code/User/settings.json]; 
+# 	mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json.backup
+# fi
+# 
+# # Move settings and symlink
+rm ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
