0157c26ddc6781362cdff1d353b30c89421d83ad
Vivek Menon <mail@vvkmnn.xyz>
2017-09-24 19:38:47 -0400

    FZF integration.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/v/.fzf/bin* ]]; then
-  export PATH="$PATH:/Users/v/.fzf/bin"
+if [[ ! "$PATH" == */Users/v/.dein/repos/github.com/junegunn/fzf/bin* ]]; then
+  export PATH="$PATH:/Users/v/.dein/repos/github.com/junegunn/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/v/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/v/.dein/repos/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/v/.fzf/shell/key-bindings.zsh"
+source "/Users/v/.dein/repos/github.com/junegunn/fzf/shell/key-bindings.zsh"
 
