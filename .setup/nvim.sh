b29abadea3f5599f1040d50e455729af2d77dbfe
Vivek Menon <mail@vvkmnn.xyz>
2017-10-04 10:17:35 -0400

    (fix): Neovim needs python support; updated.

--- .setup/nvim.sh
+++ .setup/nvim.sh
@@ -4,6 +4,10 @@
 # Install Neovim from Homebrew
 brew install neovim
 
+# Interate Python
+pip2 install neovim
+pip3 install neovim
+
 # Setup Dein (Neovim Package Manager)
 git clone https://github.com/Shougo/dein.vim ~/.dein
 
