67aace8d2be30802a37b28a6580d0d959e040566
Vivek Menon <mail@vvkmnn.xyz>
2017-10-08 21:12:18 -0400

    (feat): Moving to Vscode Insiders.

--- .setup/vscode.sh
+++ .setup/vscode.sh
@@ -2,14 +2,18 @@
 # Setup Visual Studio Code
 
 # Install Visual Studo Code
-brew cask install visual-studio-code
+brew cask install visual-studio-code-insiders
 
 # Move settings and symlink
-rm ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+rm ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+rm ~/Library/Application\ Support/Code\ -\ Insiders/User/bindings.json
+
+# Symlink existing files
+ln -s ~/.vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+ln -s ~/.vscode/bindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/bindings.json
 
 # Use `core` to install packages
-code --list-extensions
+# code --list-extensions
 
 # Vim Vscode
 code --install-extension vscodevim.vim
