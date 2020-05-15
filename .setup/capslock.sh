050b1bb8e708cd83d6438cf77ddb7fa64d2e9653
Vivek Menon <mail@vvkmnn.xyz>
2020-05-15 14:55:25 -0400

    FEAT(setup): Add more utility scripts

---
+++ .setup/capslock.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Fuck capslock
+# https://gist.github.com/tanyuan/55bca522bf50363ae4573d4bdcf06e2e
+
+case "$(uname -s)" in
+   Linux)
+
+   # xcape
+   sudo apt install -y xcape
+
+   ;;
+Darwin)
+
+    ;;
+esac
--- .setup/emacs.sh
+++ .setup/emacs.sh
@@ -1,28 +1,50 @@
-# emacs - Spacemacs!
+#!/usr/bin/env bash
+# Install and switch to Zshell
 
-# Install emacs with natural title bars
-# this one works with skhd & yabai on macOS
-brew install emacs-mac --with-spacemacs-icon --with-natural-title-bar
+case "$(uname -s)" in
+   Linux)
+   # Ubuntu PPA
+   # https://launchpad.net/~ubuntu-elisp/+archive/ubuntu/ppa
+   sudo add-apt-repository -y ppa:ubuntu-elisp/ppa && sudo apt-get update
 
-# Doom Emacs
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+   # Install Dependencies for Doom Emacs
+   sudo apt install -y git fd-find ripgrep emacs-snapshot
 
-# Doom Config
-git clone git@github.com:Vvkmnn/v.doom.d.git ~/.config/doom
-ln -s ~/.config/doom/ ~/.v.doom.d 
+   # Doom Emacs
+    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    ~/.emacs.d/bin/doom install
+    
+   ;;
+Darwin)
 
-# Doom Refresh
-doom refresh
+    # Install emacs with natural title bars
+    # this one works with skhd & yabai on macOS
+    brew install emacs-mac --with-spacemacs-icon --with-natural-title-bar
 
-# Emacs as a Daemon / Server
-# ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents
-# launchctl load ~/Library/LaunchAgents/homebrew.mxcl.emacs.plist
+    # Doom Emacs
+    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 
-# Emacs as a Client
-# brew cask install emacsclient
+    # Doom Config
+    git clone git@github.com:Vvkmnn/v.doom.d.git ~/.config/doom
+    ln -s ~/.config/doom/ ~/.v.doom.d 
 
-# https://superuser.com/questions/50095/how-can-i-run-mac-osx-graphical-emacs-in-daemon-mode
-# Use the emacs config that forks Doom!
-# brew cask install emacs-mac-spacemacs-icon
-# brew install emacs-plus --with-natural-title-bar
-# brew tap caskroom/fonts && brew cask install font-source-code-pro
+    # Doom Refresh
+    doom refresh
+
+    # Emacs as a Daemon / Server
+    # ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents
+    # launchctl load ~/Library/LaunchAgents/homebrew.mxcl.emacs.plist
+
+    # Emacs as a Client
+    # brew cask install emacsclient
+
+    # https://superuser.com/questions/50095/how-can-i-run-mac-osx-graphical-emacs-in-daemon-mode
+    # Use the emacs config that forks Doom!
+    # brew cask install emacs-mac-spacemacs-icon
+    # brew install emacs-plus --with-natural-title-bar
+    # brew tap caskroom/fonts && brew cask install font-source-code-pro
+
+
+
+    ;;
+esac
