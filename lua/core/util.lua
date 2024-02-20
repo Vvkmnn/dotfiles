1cc522ad3bcf3b891293162c5c7b5a5b84f06c42
Vivek Menon <mail@vvkmnn.xyz>
2024-02-19 20:20:35 -0500

    Squashed '.config/nvim/' content from commit 8bc9779e
    git-subtree-dir: .config/nvim
    git-subtree-split: 8bc9779ee9f05bd75d7614aaf2bc1307cbfabd0c

---
+++ lua/core/util.lua
@@ -0,0 +1,16 @@
+-- handle WSL (Windows for Linux) clipboard
+-- choco install win31yank
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "win32yank-wsl",
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
+end
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+	-- {
+	-- 	"ahmedkhalf/project.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-telescope/telescope.nvim"
+	-- 	},
+	-- 	config = function()
+	-- 		require("project_nvim").setup({})
+	-- 	end,
+	-- },
+}
