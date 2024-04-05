b01d13ea9d936712e63a98e9543fa5dc84db55a6
Vivek Menon <mail@vvkmnn.xyz>
2024-04-05 19:10:28 -0400

    FIX(tmux): Faster tmux

--- .config/tmux/random_animal.sh
+++ .config/tmux/random_animal.sh
@@ -1,12 +1,39 @@
 #!/bin/sh
-EMOJIS=(
-	"🦋" "🐢" "🦑" "🦐" "🦞" "🦀"
-	"🐙" "🦈" "🐬" "🐊" "🐅" "🦌"
-	"🦏" "🐘" "🦣" "🦙" "🐑" "🐐"
-	"🐕" "🐈" "🦚" "🦜" "🦢" "🦔"
-	"🐇" "🦦" "🦥"
-)
 
+set_random_emoji() {
+
+# Define the emojis list only if it's not already defined
+if [ -z "$EMOJIS_DEFINED" ]; then
+    EMOJIS=(
+       "🐕" "🐩" "🐈" "🐎" "🦬" "🐄" "🐖" "🐑" "🐐" "🐪" 
+       "🐫" "🦙" "🦒" "🐘" "🦣" "🦏" "🦛" "🐇" "🦔" "🦇" 
+       "🦘" "🦡" "🦃" "🐓" "🦢" "🦉" "🦤" "🦚" "🦜" "🐊" 
+       "🐢" "🦎" "🐍" "🐉" "🦕" "🦖" "🐋" "🐬" "🦭" "🐟" 
+       "🐠" "🐡" "🦈" "🐙" "🦀" "🦞" "🦑" "🐌" "🦋" "🐜" 
+       "🐝" "🪲" "🐞" "🦗" "🕷️" "🦂"
+    )
+    export EMOJIS_DEFINED=yes
+fi
+
+# Use RANDOM and modulo to select a random emoji
 RANDOM_EMOJI=${EMOJIS[$RANDOM % ${#EMOJIS[@]}]}
 
+# Set the random emoji in tmux environment
 tmux set-environment -g @random_animal "$RANDOM_EMOJI"
+}
+
+# Only call set_random_emoji when needed to reduce overhead
+# This acts as a simple form of 'lazy loading', executing the function only when the script is actually used
+set_random_emoji
+
+
+# #!/bin/sh
+# EMOJIS=(
+#    🐕 🐩 🐈 🐎 🦬 🐄 🐖 🐑 🐐 🐪 🐫 🦙 🦒 🐘 🦣 🦏 🦛 🐇 🦔 🦇 🦘 🦡 🦃 🐓 🦢 🦉 🦤  🦚 🦜 🐊 🐢 🦎 🐍 🐉 🦕 🦖 🐋 🐬 🦭 🐟 🐠 🐡 🦈 🐙 🦀 🦞 🦑 🐌 🦋 🐜 🐝 🪲 🐞 🦗  🕷️ 🦂
+# )
+#
+# # "🦚" "🦐" "🦦" "🦥" "🦌" "🦒" 
+#
+# RANDOM_EMOJI=${EMOJIS[$RANDOM % ${#EMOJIS[@]}]}
+#
+# tmux set-environment -g @random_animal "$RANDOM_EMOJI"
